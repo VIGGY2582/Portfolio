0ffa1cb65bd788cfc4eb427664ef38abb7e6ab9c
VIGGY2582 <vigneshwarachakravarthy@gmail.com>
2026-06-26 09:32:21 +0530

    Removed twitter in contact page

--- src/components/Contact/Social.jsx
+++ src/components/Contact/Social.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from "react-bootstrap";
 import {
-  AiFillGithub,
-  AiOutlineTwitter
+  AiFillGithub
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
