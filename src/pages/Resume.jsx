1dd2111e3847170c1da6478089e8b4336bab253e
VIGGY2582 <vigneshwarachakravarthy@gmail.com>
2026-06-26 08:43:31 +0530

    Resume page issues resolved

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,22 +3,19 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
-import Particle from '../components/Particle'
-import pdf from "../assets/sajib.pdf"
+import Particle from '../components/Particle';
+import pdf from "../assets/Resume.pdf";
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-const resumeLink = `https://raw.githubusercontent.com/19sajib/portfolio/main/src/assets/sajib.pdf`
-
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    
     setWidth(window.innerWidth);
   }, []);
 
@@ -26,6 +23,7 @@ const Resume = () => {
     <div>
       <Container fluid className="resume-section">
         <Particle />
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
@@ -39,7 +37,7 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -55,9 +53,10 @@ const Resume = () => {
             &nbsp;Download Resume
           </Button>
         </Row>
+
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
